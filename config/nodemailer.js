957e09f51b843b22f2c78c0eef41a67346e83222
HMiguelvilchez <miguelvilchez270910@gmail.com>
2024-08-16 16:29:03 -0500

    first commit

---
+++ config/nodemailer.js
@@ -0,0 +1,17 @@
+import nodemailer from 'nodemailer';
+
+export function createTransport() {
+    console.log('EMAIL_HOST:', process.env.EMAIL_HOST);
+    console.log('EMAIL_PORT:', process.env.EMAIL_PORT);
+    console.log('EMAIL_USER:', process.env.EMAIL_USER);
+    console.log('EMAIL_PASS:', process.env.EMAIL_PASS ? '****' : 'MISSING'); // No mostrar la contraseña completa por seguridad
+
+    return nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+}
